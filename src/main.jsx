5fcb82cb1e365fbe3cfe61fe7c77d1b48b2cf6e7
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-04-01 23:22:17 +0900

    feat : 라우팅설계

--- src/main.jsx
+++ src/main.jsx
@@ -7,35 +7,82 @@ import Movie from "./routes/movie";
 import Login from "./routes/login";
 import Signup from "./routes/singup";
 
-// import ErrorPage from "./error-page";
+const Authorization = ({ children }) => {
+  const { isLogged, routeToLoginPage } = useLoginState();
 
-const router = createBrowserRouter([
+  if (!isLogged) {
+    routeToLoginPage();
+
+    return <>로그인 페이지로 이동합니다...</>;
+  }
+
+  return <>{children}</>;
+};
+
+const RouterInfo = [
   {
     path: "/",
     element: <Root />,
-    // errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/login",
-        element: <Login />,
-        authBox: true,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-        authBox: true,
-      },
-      {
-        path: "/movies/:movieId",
-        element: <Movie />,
-        authBox: false,
-      },
-    ],
+    withAuthorization: true,
+    label: "홈",
   },
-]);
+  {
+    path: "/login",
+    element: <Login />,
+    withAuthorization: true,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    withAuthorization: true,
+  },
+];
+
+export const ReactRouterObject = createBrowserRouter(
+  RouterInfo.map((routerInfo) => {
+    return routerInfo.withAuthorization
+      ? {
+          path: routerInfo.path,
+          element: (
+            <Authorization currentPath={routerInfo.path.replace(/\/\*$/g, "")}>
+              {routerInfo.element}
+            </Authorization>
+          ),
+        }
+      : {
+          path: routerInfo.path,
+          element: routerInfo.element,
+        };
+  })
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     // errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/login",
+//         element: <Login />,
+//         authBox: true,
+//       },
+//       {
+//         path: "/signup",
+//         element: <Signup />,
+//         authBox: true,
+//       },
+//       {
+//         path: "/movies/:movieId",
+//         element: <Movie />,
+//         authBox: false,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={ReactRouterObject} />
   </React.StrictMode>
 );
